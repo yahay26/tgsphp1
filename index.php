90ef2e01169d343d15a5fdbb295bb353be620c2e
yahay26 <baksoikan736@gmail.com>
2024-03-27 01:05:11 +0700

    1

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+$name = "Gilang Akmal M" ;
+$email = "gilangakmalmaulana26@gmail.com@gmail.com";
+$phone_number = "0856 9309 3550";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Web Profile</title>
+    <link rel="stylesheet" href=""> </head>
+<body>
+    <span class="name-placeholder">NAMA : <?php echo $name; ?> <br> </span>
+    <span class="email-placeholder">EMAIL : <?php echo $email;  ?><br></span>
+    <span class="phone-placeholder"> No Telp :<?php echo $phone_number; ?><br></span>
+    </body>
+</html>
